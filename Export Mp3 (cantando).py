c2be3bd77d787f0a95082a028146802ad2ca235f
JGCampos999 <42753190+JGCampos999@users.noreply.github.com>
2019-10-24 15:20:37 -0300

    Add files via upload

---
+++ Export Mp3 (cantando).py
@@ -0,0 +1,29 @@
+from gtts import gTTS
+language = 'pt'
+bicho = ['pintinho', 'galinha', 'galo', 'perú', 'capote', 'gato',
+         'cachorro', 'cabra', 'bode','vaca','boi']
+som = ['piu', 'có', 'corócocó', 'gulu gulu', 'to fraco', 'miau',
+        'au au', 'meee', 'beee', 'mooh', 'muuh']
+letra = ''
+c = 0
+for i in range(len(bicho)):
+    s = bicho[i]
+    if (s[len(s)-1:] == 'a'):
+        s = 'uma'
+    else:
+        s = 'um'
+    letra+= ('Lá em casa tinha {} {}\nLá em casa tinha {} {}\n'.format(s, bicho[i],s, bicho[i]))
+    for j in range(c, 0, -1):
+        s = bicho[j]
+        if (s[len(s)-1:] == 'a'):
+            s = 'a'
+        else:
+            s = 'o'
+        letra+= ('E {} {} {}\n' .format(s, bicho[j], som[j]))
+    for x in range(2):
+        letra+= ('E o pintinho piu\n')
+    c-= -1
+    
+output = gTTS(text=letra,lang=language,slow=False)
+output.save('output.mp3')
+
